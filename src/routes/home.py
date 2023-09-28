87dc039c92bc7acb10df504148cc48ba097d864f
freelancing-solutions <mobiusndou@gmail.com>
2023-09-28 13:09:41 +0200

    fixed template links

--- src/routes/home.py
+++ src/routes/home.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Blueprint, render_template
 
 
@@ -7,6 +9,7 @@ home_route = Blueprint('home', __name__)
 @home_route.get('/')
 async def get_home():
     image_filenames = ['images/gallery/image_1.jpg', 'images/gallery/image_2.jpg', 'images/gallery/image_3.jpg', 'images/gallery/image_4.jpg']
-    return render_template('index.html', image_filenames=image_filenames)
+    image_filename = random.choice(image_filenames)
+    return render_template('index.html', image_filenames=image_filenames, image_filename=image_filename)
 
 
