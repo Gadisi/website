aa21694d07b0503b889d133ac8c8aeba5487a93a
freelancing-solutions <mobiusndou@gmail.com>
2023-09-28 11:21:20 +0200

    fixed template links

--- src/routes/home.py
+++ src/routes/home.py
@@ -6,6 +6,7 @@ home_route = Blueprint('home', __name__)
 
 @home_route.get('/')
 async def get_home():
-    return render_template('index.html')
+    image_filenames = ['images/gallery/image_1.jpg', 'images/gallery/image_2.jpg', 'images/gallery/image_3.jpg', 'images/gallery/image_4.jpg']
+    return render_template('index.html', image_filenames=image_filenames)
 
 
